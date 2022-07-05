3dce191252fd96f8dac421f0f82c3a6c1b254a88
danugarri <danugarri@hotmail.com>
2022-07-06 01:18:51 +0200

    added linking bar

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,5 +2,16 @@ import React from 'react';
 import './Card.scss';
 
 export const Card = ({ children }) => {
-  return <section id='card'>{children}</section>;
+  return (
+    <section id='card'>
+      <div id='banner'>
+        <div className='detail'>1</div>
+        <div className='linking-bar'></div>
+        <div className='detail'>2</div>
+        <div className='linking-bar'></div>
+        <div className='detail'>3</div>
+      </div>
+      {children}
+    </section>
+  );
 };
