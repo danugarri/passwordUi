b772f41b954b89c736a1ac84d0127802e339e531
danugarri <danugarri@hotmail.com>
2022-07-06 02:48:10 +0200

    adding logic for banner

--- src/components/card/banner/Banner.jsx
+++ src/components/card/banner/Banner.jsx
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 
 export const Banner = () => {
+  const [hover1, setHover1] = useState(false);
+  const [hover2, setHover2] = useState(false);
+  const [hover3, setHover3] = useState(false);
+
   return (
-    <div id='banner'>
-      <div className='detail'>1</div>
+    <section id='banner'>
+      <div className='detail-container'>
+        <div
+          className='detail'
+          onClick={() => {
+            setHover1(true);
+            setHover2(false);
+            setHover3(false);
+          }}
+        >
+          1
+        </div>
+        {hover1 && <ArrowDropUpIcon color='error' className='arrow-icon' />}
+      </div>
       <div className='linking-bar'></div>
-      <div className='detail'>2</div>
+      <div className='detail-container'>
+        <div
+          className='detail'
+          onClick={() => {
+            setHover1(false);
+            setHover2(true);
+            setHover3(false);
+          }}
+        >
+          2
+        </div>
+        {hover2 && <ArrowDropUpIcon color='error' className='arrow-icon' />}
+      </div>
       <div className='linking-bar'></div>
-      <div className='detail'>3</div>
-    </div>
+      <div className='detail-container'>
+        <div
+          className='detail'
+          onClick={() => {
+            setHover1(false);
+            setHover2(false);
+            setHover3(true);
+          }}
+        >
+          3
+        </div>
+        {hover3 && <ArrowDropUpIcon color='error' className='arrow-icon' />}
+      </div>
+    </section>
   );
 };
