0ceec212857a964539a61508cf07734bb43556e5
danugarri <danugarri@hotmail.com>
2022-07-07 02:31:21 +0200

    added image an styles

--- src/views/ProductInformation/ProductInformation.jsx
+++ src/views/ProductInformation/ProductInformation.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-
+import { useTranslation } from 'react-i18next';
+import './ProductInformation.scss';
+import step1 from '../../assets/img/steps/step1.PNG';
 export const ProductInformation = () => {
-  return <section>{t('steps.step1.header')}</section>;
+  const { t } = useTranslation(['global']);
+  return (
+    <section className='steps-container'>
+      <p className='password-manager'>{t('steps.step1.header')}</p>
+      <img alt='step1' src={step1} />
+    </section>
+  );
 };
