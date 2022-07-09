ef28af2d1a9663dfbd8914eb9f0f3c6c5f154774
danugarri <danugarri@hotmail.com>
2022-07-09 03:49:41 +0200

    useCheck logic

--- src/app/hooks/useCheck.js
+++ src/app/hooks/useCheck.js
@@ -4,12 +4,11 @@ export const useCheck = (password, secondPassword) => {
   const [disabled, setDisabled] = useState();
 
   useEffect(() => {
-    password === secondPassword &&
-    password.length === secondPassword.length &&
-    password !== '' &&
-    secondPassword !== ''
-      ? setDisabled(false)
-      : setDisabled(true);
+    const passwordMatches =
+      password === secondPassword && password.length === secondPassword.length;
+    const passwordsNotEmpty = password !== '' && secondPassword !== '';
+    // Conditions
+    passwordMatches && passwordsNotEmpty ? setDisabled(false) : setDisabled(true);
   }, [password, secondPassword]);
   return [disabled];
 };
